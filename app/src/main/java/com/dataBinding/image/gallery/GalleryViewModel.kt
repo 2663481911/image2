46bf87266d8c1a3ae3ba4a2ee4b730add3d58733
lss <2663481911@qq.com>
2020-11-23 20:44:39 +0800

    Default Changelist

---
+++ app/src/main/java/com/dataBinding/image/gallery/GalleryViewModel.kt
@@ -0,0 +1,81 @@
+package com.dataBinding.image.gallery
+
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.dataBinding.image.analyzeRule.AnalyzeRule
+import com.dataBinding.image.analyzeRule.RuleUtil
+import com.dataBinding.image.model.HomeData
+import com.dataBinding.image.model.Rule
+
+class GalleryViewModel(application: Application) : AndroidViewModel(application) {
+    private val _photoListLive = MutableLiveData<List<HomeData>>()
+    private var ruleUtil:RuleUtil? = null
+    val photoListLive:LiveData<List<HomeData>>
+        get() = _photoListLive
+    private var getVale = false
+    private var pageNum = 1
+    var isRefresh:Boolean = true
+
+    fun getHtml(url: String, isTopRefresh:Boolean=true){
+//      用于第一次加载把位置调到0
+        if (_photoListLive.value == null)
+            this.isRefresh = true
+        else this.isRefresh = isTopRefresh
+
+        if (ruleUtil == null){
+            getRuleUtil()
+        }
+        
+        // 用于防止加载多次
+        if (getVale) return
+        getVale = true
+        StringRequest(
+            Request.Method.GET,
+            url.replace("@page", pageNum.toString()),
+            {
+                getVale = false
+
+                pageNum += 1
+                val homeDataList = ruleUtil?.getHomeDataList(it)
+                Log.d("homeDataListNum", homeDataList?.size.toString())
+                if (!isTopRefresh) {       // 不是顶部刷新
+                    val values = _photoListLive.value?.toMutableList()
+                    for (value in homeDataList!!){
+                       values?.add(value)
+                   }
+                    _photoListLive.value = values
+                }
+                else _photoListLive.value = homeDataList
+            },
+            {
+                Log.d("getHtml", "错误${it}")
+            },
+        ).also {
+            VolleySingleton.getRequestQueue(getApplication()).add(it)
+        }
+    }
+
+    private fun getUrl():String{
+        return "https://gank.io/api/v2/data/category/Girl/type/Girl/page/${(0..5).random()}/count/20"
+    }
+
+
+    private fun getRuleUtil(){
+        val rule = Rule()
+        rule.homeList = ".work-thumbnail"
+        rule.homeHref = "a@href"
+        rule.homeSrc = "img@abs:src"
+        rule.homeTitle = "div.title@text"
+
+        rule.imagePageList = "#imgs_json@text"
+        rule.imagePageSrc = "@json:$.[*]..img"
+        rule.imageUrlReplaceByJS = "imgSrc = 'http://imgoss.cnu.cc/' + imgSrc;"
+        ruleUtil = RuleUtil(rule, AnalyzeRule())
+    }
+
+}
